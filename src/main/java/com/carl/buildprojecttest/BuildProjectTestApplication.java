8d68b80e54cfc13c845a660590e2dd00b575288a
cailei <414132181@qq.com>
2020-12-14 11:10:05 +0800

    111

---
+++ src/main/java/com/carl/buildprojecttest/BuildProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.carl.buildprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildProjectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildProjectTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/carl/buildprojecttest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.carl.buildprojecttest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BuildProjectTestApplication.class);
+    }
+
+}
